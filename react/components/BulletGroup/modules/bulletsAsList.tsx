aa8ca1efb0f10965d7276714dd9fbedc21a2f522
alann03 <alanhuismann03@gmail.com>
2023-02-12 20:27:43 -0300

    feat: componente finalizado

---
+++ react/components/BulletGroup/modules/bulletsAsList.tsx
@@ -0,0 +1,27 @@
+import React from 'react';
+import { BulletsSchema } from '../BulletTypes';
+import Bullet from '../Bullet';
+
+export const getBulletsAsTSXList = (
+    bullets: BulletsSchema
+) => (
+    bullets.map((bullet: any, index) => {
+        console.log("Mi bullet es: ", bullet)
+        return <Bullet key={index}
+            src={bullet.image}
+            titleBullet={bullet.titleBullet}
+            link={
+                bullet.link
+                    ?
+                    bullet.link
+                    :
+                    {
+                        url: "",
+                        attributeNofollow: false,
+                        atributeTittle: "",
+                        openNewTab: false,
+                        newTab: false
+                    }
+            } />
+    })
+)
